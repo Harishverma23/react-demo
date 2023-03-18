d002a37a066e3cf805eb77604a96b4bc462c8388
Harish Verma <246745@ust.com>
2023-03-18 22:30:43 +0530

    main branch first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router} from 'react-router-dom';
-import Login from './components/Login';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PageRoutes from './components/PageRoutes';
